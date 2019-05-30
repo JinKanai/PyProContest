def31c5b7e0a6d47869385bfd34c22922075fb6f
jin kanai <jin@kanaisan.com>
2019-05-30 02:01:17 +0000

    add comments

--- nabeatsu_practice/sekainonabeatsu.py
+++ nabeatsu_practice/sekainonabeatsu.py
@@ -4,42 +4,69 @@ class SekainoNabeatsu:
     与えられた数までのナベアツ数をリストで返す
     """
 
-    def __init__(self, upper_limit):
-        self.places = ['','ジュウ','ヒャク','セン','マン']
-        self.numbers = ['','イチ','ニ','サ～ン','ヨン','ゴ','ロク','ナナ','ハチ','キュウ']
-        self.upper_limit = upper_limit + 1
+    def __init__(self, stop_number):
+        self.places = ['', 'ジュウ', 'ヒャク', 'セン', 'マン']
+
+        # ３は必ずアホになるのであらかじめ～を付けとく
+        self.numbers = ['', 'イチ', 'ニ', 'サ～ン',
+                        'ヨン', 'ゴ', 'ロク', 'ナナ', 'ハチ', 'キュウ']
+        self.stop_number = stop_number + 1
         self.nabeatsu_number = []
         self._main_loop()
 
     def _main_loop(self):
-        for i in range(1,self.upper_limit):
-            n=i
-            if i%3 == 0 or '3' in str(i):
-                n=self._make_nabeatsu(i)
+        for i in range(1, self.stop_number):
+            n = i
+            if i % 3 == 0 or '3' in str(i):
+                n = self._make_nabeatsu(i)
             self.nabeatsu_number.append(n)
 
-    def _make_nabeatsu(self,org_num):
+    def _make_nabeatsu(self, org_num):
+        """
+        渡された数をナベアツ数に変換
+        """
         result = ''
         str_num = str(org_num)
+
+        # 桁数を出しとく
         digits = len(str_num)
-        for i,num in enumerate(str_num):
+
+        for i, num in enumerate(str_num):
+            # 今処理している桁
             current_place = (digits-i)-1
+
+            # イチヒャク、イチセンとかいう記載にならないようにする判定if文
+            # 取り出してる数字が１でなくかつ2桁以上の数字のときのみナベアツ数に変換
+            # ただし処理している桁が1の位の時はナベアツ数に変換
             if current_place == 0 or num != '1' and digits != 1:
                 result += self.numbers[int(num)]
             result += self.places[current_place]
         return(self._ahonize(result))
 
-    def _ahonize(self,nabeatsu_number):
+    def _ahonize(self, nabeatsu_number):
+        """
+        ナベアツ数をアホにする処理
+        """
+
+        # 先に末尾に！！を付けて、次のif文で一の位を見つけやすくする
         r = nabeatsu_number + '！！'
+
+        # 1の位のサ～ンに二重で～が付かないようにする判定if文
         if 'サ～ン！！' not in r:
-            p  = len(nabeatsu_number) -1
+            p = len(nabeatsu_number) - 1
+            # 1の位以外のナベアツ数をアホにするために、最後の1文字目の前に～を挿入。
+            # ！！が入っていると場所がうまく取れないため、！！を付ける前の引数を利用
             r = nabeatsu_number[:p] + '～' + nabeatsu_number[p:] + '！！'
         return r
 
+
 def main():
+    # 実行したい数をSekainoNabeatuクラスに渡してインスタンス化
     sekaino = SekainoNabeatsu(1000)
+
     for i in sekaino.nabeatsu_number:
         print(i)
+
 
 if __name__ == '__main__':
     main()
