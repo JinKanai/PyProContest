9a3977e5e73a703150134a9c474fd70e437b6483
jin kanai <inumayugeh@gmail.com>
2019-06-02 17:21:28 +0900

    irohani OK

---
+++ irohani-generator/irohani.py
@@ -0,0 +1,31 @@
+class IrohaniGenerator:
+
+    def __init__(self):
+        self.irohanibun = set()
+        self.main_loop()
+
+    def main_loop(self):
+        import random
+        while len(self.irohanibun) < 48:
+            target = random.randrange(0x3042, 0x3094)
+            if self.can_listed(target):
+                self.irohanibun.add(chr(target))
+
+    def can_listed(self, target):
+        import jaconv
+        ignore_list = [0x3043, 0x3045, 0x3047, 0x3049, 0x3063, 0x3083, 0x3085, 0x3087, 0x308e]
+        katakana = jaconv.hira2hkata(chr(target))
+        r = True if len(katakana) == 1 and target not in ignore_list else False
+        return r
+
+
+def main():
+    iroha = IrohaniGenerator()
+    for i, kana in enumerate(iroha.irohanibun, start=1):
+        print(kana, end='')
+        if i % 5 == 0:
+            print('')
+
+
+if __name__ == '__main__':
+    main()
